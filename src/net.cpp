fe93f65299d3ae3afb0aa59d75306410c0ba3a6d
bjornc-project <39172695+bjornc-project@users.noreply.github.com>
2018-05-24 10:31:49 -0400

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1507,7 +1507,7 @@ void ThreadMapPort()
             }
         }
 
-        std::string strDesc = "Litecoin " + FormatFullVersion();
+        std::string strDesc = "Bjornc " + FormatFullVersion();
 
         try {
             while (true) {
